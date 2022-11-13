3182d0fca5893b1109a07a186ca709728781ecbf
majid <majid_ashoori_zad@yahoo.com>
2022-11-13 11:33:17 +0330

    header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>about</p>
+      <p>contact</p>
+      <p>profile</p>
+    </div>
+  );
+};
+
+export default Header;
